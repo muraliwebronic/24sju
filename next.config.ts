05d41d6f702c2fe94aa9f9d322578fce1a3b1fe7
muraliwebronic <muralikrishna.webronic@gmail.com>
2025-12-24 13:33:30 +0530

    fonts

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
   reactCompiler: true,
+
+  // GitHub Pages base path
+  basePath: "/24sju",
+  assetPrefix: "/24sju/",
 };
 
 export default nextConfig;
